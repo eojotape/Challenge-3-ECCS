01dbe43bea48646c340520835a2f7d98bf881470
eojotape <126830164+eojotape@users.noreply.github.com>
2023-09-17 17:48:18 -0300

    Update CodigoFonte-C3-EC&CS.ino

--- CodigoFonte-C3-EC&CS.ino
+++ CodigoFonte-C3-EC&CS.ino
@@ -1,3 +1,10 @@
+#include "Arduino.h"
+#include "WiFi.h"
+
+
+char nomeWifi[] = "Iphone Jotape";
+char senhaWifi[] = "12345678";
+
 int ledPin1 = 9;    // pino do LED 1
 int ledPin2 = 10;   // pino do LED 2
 int ledPin3 = 11;   // pino do LED 3
@@ -8,6 +15,8 @@ void setup() {
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
   pinMode(potPin, INPUT);
+  Serial.begin(9600);
+  init_wifi();
 }
 
 void loop() {
@@ -16,4 +25,15 @@ void loop() {
   analogWrite(ledPin1, ledValue);      // ajustar o brilho do LED 1
   analogWrite(ledPin2, ledValue);      // ajustar o brilho do LED 2
   analogWrite(ledPin3, ledValue);      // ajustar o brilho do LED 3
+  
+  Serial.println("Conectando WiFi");
+  Serial.print("Wifi: ");
+  Serial.println(nomeWifi);
+  WiFi.begin(nomeWifi,senhaWifi);
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+  }
+  Serial.println("WiFi Conectado");
+  Serial.print("Meu IP eh: ");
+  Serial.println(WiFi.localIP());
 }
